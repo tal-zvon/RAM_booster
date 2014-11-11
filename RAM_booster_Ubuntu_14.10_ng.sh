0641f34e09a8085ee17ad03fe095b199cc8dd9c5
Tal <tal@server.fake>
2014-11-11 16:47:27 -0700

    Make rsync use human readable byte counter at boot

--- RAM_booster_Ubuntu_14.10_ng.sh
+++ RAM_booster_Ubuntu_14.10_ng.sh
@@ -333,7 +333,7 @@ sudo sed -i 's#\(if \[ "\${freespace}" -lt "\${size}" ]\)#\1 2>/dev/null#' /lib/
 #sudo sed -i 's#if ${PATH_ID} "${sysfs_path}"#if ${PATH_ID} "${sysfs_path}" 2>/dev/null#g' /lib/live/boot/9990-misc-helpers.sh 2>/dev/null
 
 #Make rsync at boot use human readable byte counter
-#sudo sed -i 's/rsync -a --progress/rsync -a -h --progress/g' /lib/live/boot/9990-toram-todisk.sh 2>/dev/null
+sudo sed -i 's/rsync -a --progress/rsync -a -h --progress/g' /lib/live/boot/9990-toram-todisk.sh 2>/dev/null
 
 #Fix boot messages
 #sudo sed -i 's#\(echo " [*] Copying $MODULETORAMFILE to RAM" 1>/dev/console\)#\1\
