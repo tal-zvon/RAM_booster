e03ba834fd1f09a588f5e2f65c17f9730b4d17ed
Tal <tal@server.fake>
2014-11-11 08:18:20 -0700

    Added section that tells user how much RAM they should have

--- RAM_booster_Ubuntu_14.10_ng.sh
+++ RAM_booster_Ubuntu_14.10_ng.sh
@@ -536,3 +536,17 @@ else
         echo "Squashfs image created successfully."
 fi
 
+###########################################
+# Tell user how much RAM they should have #
+###########################################
+
+#Find out how big the squashfs image ended up being
+Image_Size=$(sudo du -h /live/filesystem.squashfs | awk '{ print $1 }')
+
+#Tell user how much RAM they should have
+ECHO "The size of the image is $Image_Size. This MUST fit in your total RAM, with room to spare. If it does not, you either need to buy more RAM, or manually remove unimportant packages from your OS until the image fits."
+echo
+
+ECHO "Note: Do NOT format your original OS that you made the RAM Session out of, as the squashfs image still resides there. So does /var/squashfs, the folder the image gets recreated from everytime you make any changes to the RAM Session through the update scripts. You should be able to shrink the partition with your original OS however in order to save space."
+
+ECHO "Also, if you switch between your original OS and your RAM Session a lot, and forget which one you are in, do an 'ls /'. If you see the /Original_OS file, you are in the original OS. If you see the /RAM_Session file, you are in the RAM Session."
