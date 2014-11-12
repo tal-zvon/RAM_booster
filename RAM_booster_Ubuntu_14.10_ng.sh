0a8830eadb296459d4af38a13a800527d70d6ba9
Tal <tal@server.fake>
2014-11-12 08:07:33 -0700

    Removed udevadm fix that no longer does anything

--- RAM_booster_Ubuntu_14.10_ng.sh
+++ RAM_booster_Ubuntu_14.10_ng.sh
@@ -344,9 +344,6 @@ sudo sed -i 's/\(size=$( expr $(ls -la ${MODULETORAMFILE} | awk '\''{print $5}'\
 #Hide 'sh:bad number' error on boot
 sudo sed -i 's#\(if \[ "\${freespace}" -lt "\${size}" ]\)#\1 2>/dev/null#' /lib/live/boot/9990-toram-todisk.sh 2>/dev/null
 
-#Suppress udevadm output
-#sudo sed -i 's#if ${PATH_ID} "${sysfs_path}"#if ${PATH_ID} "${sysfs_path}" 2>/dev/null#g' /lib/live/boot/9990-misc-helpers.sh 2>/dev/null
-
 #Make rsync at boot use human readable byte counter
 sudo sed -i 's/rsync -a --progress/rsync -a -h --progress/g' /lib/live/boot/9990-toram-todisk.sh 2>/dev/null
 
