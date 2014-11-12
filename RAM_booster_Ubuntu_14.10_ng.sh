aa0fb4f831c218c6a0041175bef91c88cbd02821
Tal <tal@server.fake>
2014-11-11 21:37:24 -0700

    Added code to clear screen before and after rsync at boot

--- RAM_booster_Ubuntu_14.10_ng.sh
+++ RAM_booster_Ubuntu_14.10_ng.sh
@@ -353,9 +353,12 @@ sudo sed -i 's/rsync -a --progress/rsync -a -h --progress/g' /lib/live/boot/9990
 #Add line right before rsync runs that says the total filesize of the squashfs image
 #It looks like this:
 # * filesystem.squashfs: 1.19G
-sudo sed -i 's#\(echo " [*] Copying $MODULETORAMFILE to RAM" 1>/dev/console\)#\1\
+sudo sed -i 's#\(echo " [*] Copying $MODULETORAMFILE to RAM" 1>/dev/console\)#echo -ne "\\033c" 1>/dev/console\
+				\1\
 				echo -n " * `basename $MODULETORAMFILE` is: " 1>/dev/console\
 				rsync -h -n -v ${MODULETORAMFILE} ${copyto} | grep "total size is" | grep -Eo "[0-9]+[.]*[0-9]*[mMgG]" 1>/dev/console\
+				sleep 1
+				echo -ne "\\033c" 1>/dev/console\
 				echo 1>/dev/console#g' /lib/live/boot/9990-toram-todisk.sh 2>/dev/null
 
 #Fix the "can't create /root/etc/fstab.d/live: nonexistent directory" error at boot
