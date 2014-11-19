b3211460819d716231b0003ccff3c26e646e8e4c
Tal <tal@server.fake>
2014-11-18 18:01:31 -0700

    Added script to create symlinks to files in repo during development

---
+++ dev_script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo rm -v /usr/sbin/rchroot /usr/sbin/rupdate /usr/sbin/rupgrade
+sudo rm -v /var/lib/ram_booster/rlib
+
+sudo ln -s -v /home/test/Documents/RAM_Booster/extras_14.10/rupdate /usr/sbin/
+sudo ln -s -v /home/test/Documents/RAM_Booster/extras_14.10/rupgrade /usr/sbin/
+sudo ln -s -v /home/test/Documents/RAM_Booster/extras_14.10/rchroot /usr/sbin/
+sudo ln -s -v /home/test/Documents/RAM_Booster/extras_14.10/rlib /var/lib/ram_booster/
+
+sudo chmod -v a+x /home/test/Documents/RAM_Booster/extras_14.10/rupdate
+sudo chmod -v a+x /home/test/Documents/RAM_Booster/extras_14.10/rupgrade
+sudo chmod -v a+x /home/test/Documents/RAM_Booster/extras_14.10/rchroot
+
+cd /home/test/Documents/RAM_Booster/
+echo "Telling RAM_Booster repo to ignore exec permissions"
+git config core.fileMode false
