cc0f654d78db83c205be3819b59b3cd07fbc02df
Tal <tal@server.fake>
2014-11-27 11:41:35 -0700

    Removed line from dev_script.sh

--- dev_script.sh
+++ dev_script.sh
@@ -6,12 +6,10 @@ sudo ln -s -v /home/test/Documents/RAM_Booster/extras_14.10/rupdate /usr/sbin/
 sudo ln -s -v /home/test/Documents/RAM_Booster/extras_14.10/rupgrade /usr/sbin/
 sudo ln -s -v /home/test/Documents/RAM_Booster/extras_14.10/rchroot /usr/sbin/
 sudo ln -s -v /home/test/Documents/RAM_Booster/extras_14.10/rlib /var/lib/ram_booster/
-sudo ln -s -v /home/test/Documents/RAM_Booster/extras_14.10/postinst.d/version_check.sh /etc/kernel/postinst.d/
 
 sudo chmod -v a+x /home/test/Documents/RAM_Booster/extras_14.10/rupdate
 sudo chmod -v a+x /home/test/Documents/RAM_Booster/extras_14.10/rupgrade
 sudo chmod -v a+x /home/test/Documents/RAM_Booster/extras_14.10/rchroot
-sudo chmod -v a+x /home/test/Documents/RAM_Booster/extras_14.10/postinst.d/version_check.sh
 
 cd /home/test/Documents/RAM_Booster/
 echo "Telling RAM_Booster repo to ignore exec permissions"
