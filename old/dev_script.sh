5758c19c6acfe0744638b3b606f15bc35f8f01b3
Tal <tal@server.fake>
2014-12-11 15:38:56 -0700

    Updated dev_script.sh

--- old/dev_script.sh
+++ old/dev_script.sh
@@ -2,14 +2,14 @@
 sudo rm -v /usr/sbin/redit /usr/sbin/rupdate /usr/sbin/rupgrade
 sudo rm -v /var/lib/ram_booster/rlib
 
-sudo ln -s -v /home/test/Documents/RAM_Booster/extras_14.10/rupdate /usr/sbin/
-sudo ln -s -v /home/test/Documents/RAM_Booster/extras_14.10/rupgrade /usr/sbin/
-sudo ln -s -v /home/test/Documents/RAM_Booster/extras_14.10/redit /usr/sbin/
-sudo ln -s -v /home/test/Documents/RAM_Booster/extras_14.10/rlib /var/lib/ram_booster/
+sudo ln -s -v /home/test/Documents/RAM_Booster/extras_14.10/RAM_Session/rupdate /usr/sbin/
+sudo ln -s -v /home/test/Documents/RAM_Booster/extras_14.10/RAM_Session/rupgrade /usr/sbin/
+sudo ln -s -v /home/test/Documents/RAM_Booster/extras_14.10/RAM_Session/redit /usr/sbin/
+sudo ln -s -v /home/test/Documents/RAM_Booster/extras_14.10/RAM_Session/rlib /var/lib/ram_booster/
 
-sudo chmod -v a+x /home/test/Documents/RAM_Booster/extras_14.10/rupdate
-sudo chmod -v a+x /home/test/Documents/RAM_Booster/extras_14.10/rupgrade
-sudo chmod -v a+x /home/test/Documents/RAM_Booster/extras_14.10/redit
+sudo chmod -v a+x /home/test/Documents/RAM_Booster/extras_14.10/RAM_Session/rupdate
+sudo chmod -v a+x /home/test/Documents/RAM_Booster/extras_14.10/RAM_Session/rupgrade
+sudo chmod -v a+x /home/test/Documents/RAM_Booster/extras_14.10/RAM_Session/redit
 
 cd /home/test/Documents/RAM_Booster/
 echo "Telling RAM_Booster repo to ignore exec permissions"
